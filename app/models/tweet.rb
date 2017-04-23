c350981a820bc8ad93aa9afdc5804ec092b65d1f
akira <akira.kubotera+cloud9@gmail.com>
2017-04-23 07:21:36 +0000

    機能だけは一通り

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,6 @@
 class Tweet < ActiveRecord::Base
     validates :content, presence: true
+    validates :content, length: { maximum: 140 } 
+    #index表示順をCreateの時間で並び替え(update時に順番が崩れない)
+    default_scope -> { order(created_at: :desc) }
 end
